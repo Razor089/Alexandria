a72e802f2443dff692593f8c1febb716eff43ae4
Razor089 <cosimod089@gmail.com>
2016-08-01 14:20:55 +0200

    Creazione della terza vista

---
+++ Alexandria/ThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ThirdViewController.swift
+//  Alexandria
+//
+//  Created by Cosimo Davide Viggiano on 01/08/16.
+//  Copyright © 2016 Cosimo Davide Viggiano. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
