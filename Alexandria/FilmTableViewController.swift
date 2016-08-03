e62f7a82b1dd497428d2f2944ac782aba33f9460
Razor089 <cosimod089@gmail.com>
2016-08-03 12:22:34 +0200

    risolti alcuni bug, inserimento dell'icona dell'app, aggiunta della vista per i Film

---
+++ Alexandria/FilmTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FilmTableViewController.swift
+//  Alexandria
+//
+//  Created by Cosimo Davide Viggiano on 03/08/16.
+//  Copyright © 2016 Cosimo Davide Viggiano. All rights reserved.
+//
+
+import UIKit
+
+class FilmTableViewController: UITableViewController {
+    
+}
